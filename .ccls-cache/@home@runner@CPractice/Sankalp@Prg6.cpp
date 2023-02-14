e5c984bccd5d7b23326366e89165758dbe911b41
Sankalp M shet <109754679+Mist54@users.noreply.github.com>
2023-02-14 14:36:38 +0000

    Prg6 as been updated

--- .ccls-cache/@home@runner@CPractice/Sankalp@Prg6.cpp
+++ .ccls-cache/@home@runner@CPractice/Sankalp@Prg6.cpp
@@ -2,12 +2,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-  int num1 = 6, num2 = 4, temp = 0;
-  int arr[] = {1, 3, 5, 6, 4, 7, 9};
+  int num1 = 6, pos = 4, temp = 0;
+  int arr[] = {1,2,3,6,5,4,7};
   int size = sizeof(arr) / sizeof(arr[0]);
   for (int i = 0; i < size; i++) {
-    if (arr[num2 - 1] == num1) {
-      arr[num2 - 1] = num2;
+    if (arr[pos - 1] == num1) {
+      arr[pos - 1] = pos;
       arr[num1 - 1] = num1;
     }
     cout << arr[i];
